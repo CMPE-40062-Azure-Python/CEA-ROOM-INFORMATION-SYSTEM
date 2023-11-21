d063ad925354918ca308c57ee1f171e9af7f0d84
jei16 <jhoncarlodc41603@gmail.com>
2023-11-21 13:54:08 +0800

    Initialized 'Rooms' Page with navbar (back button functioning)

--- website/views.py
+++ website/views.py
@@ -14,6 +14,10 @@ def student_rep():
 def student_rep_home():
     return render_template("student-rep-home.html")
 
+@views.route('/student-rep-home-rooms')
+def student_rep_home_rooms():
+    return render_template("student-rep-home-rooms.html")
+
 @views.route('/admin')
 def admin():
     return render_template("admin-login.html")
