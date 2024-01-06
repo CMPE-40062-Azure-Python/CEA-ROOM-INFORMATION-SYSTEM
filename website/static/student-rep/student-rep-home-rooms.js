dbe3d8c3e3fc708c31379f02ec534d141df5bbf9
BryceMadelo <brycemadelo07@gmail.com>
2024-01-06 22:51:19 +0800

    Room details Modification

--- website/static/student-rep/student-rep-home-rooms.js
+++ website/static/student-rep/student-rep-home-rooms.js
@@ -207,13 +207,15 @@ $(document).ready(function () {
 // Function to open the modal
 function openModal() {
     document.getElementById('room313Modal').style.display = 'block';
+
+    document.body.classList.add('modal-open');
   }
   
-  // Function to close the modal
   function closeModal() {
     document.getElementById('room313Modal').style.display = 'none';
+    
+    document.body.classList.remove('modal-open');
   }
   
-// Event listener for clicking Room 313
-document.getElementById('room313').addEventListener('click', openModal);
-
+  // Event listener for clicking Room 313
+  document.getElementById('room313').addEventListener('click', openModal);
